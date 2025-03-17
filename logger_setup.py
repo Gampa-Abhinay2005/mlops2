a838914925ec873c40d7fd8a602a3d3199dcbdc1
Gampa-Abhinay2005 <abhinaygampa8@gmail.com>
2025-03-17 15:48:55 +0530

    changed  logger_setup

--- logger_setup.py
+++ logger_setup.py
@@ -6,3 +6,10 @@ os.makedirs("logs", exist_ok=True)
 logger.add("logs/gesture_logs.log", rotation="00:00", compression="zip", level="INFO")
 
 logger.info("Logging initialized in logs/ folder")
+
+logger.add("gesture_logs.log", rotation="00:00", compression="zip", level="INFO")
+
+logger.info("Logger initialized")
+
+# Export logger for use in other scripts
+__all__ = ["logger"]
